5633e423128d927d60b12aaad048e6ad2f663ea5
blasjesus881-dotcom <blasjesus881@gmail.com>
2025-11-10 00:40:47 -0500

    dddd

--- src/main/java/com/example/practica1/repository/UsuarioRepository.java
+++ src/main/java/com/example/practica1/repository/UsuarioRepository.java
@@ -2,10 +2,11 @@ package com.example.practica1.repository;
 
 import com.example.practica1.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByLogin(String login);
 }
